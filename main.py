88d8c4959662a11c9fc203116b2edaa4f07c95f6
Milad Vazan <165850937+milad-ai@users.noreply.github.com>
2025-08-24 21:26:48 +0330

    Update main.py

--- main.py
+++ main.py
@@ -327,4 +327,5 @@ def run():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-Thread(target=run).start() updater.idle()
+Thread(target=run).start() 
+updater.idle()
